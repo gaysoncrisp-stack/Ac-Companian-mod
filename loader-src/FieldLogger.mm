9cce8c071ab2c0f4d5182de75f43bd1556252213
Crisp <245650982+gaysoncrisp-stack@users.noreply.github.com>
2025-11-25 09:17:43 -0700

    Update FieldLogger.mm

--- loader-src/FieldLogger.mm
+++ loader-src/FieldLogger.mm
@@ -4541,6 +4541,12 @@ void initStuff(MemoryFileInfo framework)
 
     std::string s = il2cpp_string_to_std(sObj, string_chars, string_length);
     NSLog(@"[Kitty] ISession.ToString => %s", s.c_str());
+
+    auto m_get_RefreshToken = s_get_method_from_name(Session, "get_RefreshToken", 0);
+    auto get_RefreshToken = (Il2CppString(*)(Il2CppObject*))STRIP_FP(m_get_RefreshToken->methodPointer);
+
+    std::string refreshs = il2cpp_string_to_std(get_RefreshToken(iSession), string_chars, string_length);
+    NSLog(@"[Kitty] ISession Refresh Token => %s", refreshs.c_str());
     
     int tries = 0;
 
